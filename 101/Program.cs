e7cc12be468dffa4efac991f45b84a314ad0b577
Lorenz <lornz.scheithauer@gmail.com>
2026-07-06 08:58:02 +0200

    Aufgabe 101 und 102 hinzugefuegt

---
+++ 101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text; // Bitte nicht löschen!
+
+namespace a101;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        print("natürliche Sprachen sind oft kontextanbängig und mehrdeutig, weswegen sie für den computer nicht geeignet sind, da er eine eindeutige Sprache braucht ");
+        print("");
+    }
+}
---
+++ 102/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text; // Bitte nicht löschen!
+
+namespace 102;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Behebung des Problems der Eingabe von z.B. Umlauten - Bitte nicht löschen!
+        Console.InputEncoding = Encoding.UTF8;
+
+        // Und ab hier kommt Ihr eigener Quellcode...
+        Console.WriteLine("Hallo, Wildau!");
+    }
+}
